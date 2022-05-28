65a15b65c531766412c2b5a712a526f5b759e80f
James Neumann <41788164+JamesNeumann@users.noreply.github.com>
2022-05-28 23:42:19 +0200

    Intial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import OpenSeadragon, { Options } from 'openseadragon';
+import './style.css';
+
+const options: Options = {
+  id: 'viewer',
+  prefixUrl: '//openseadragon.github.io/openseadragon/images/',
+  tileSources: '../assets/fuffi/dzi.dzi',
+  animationTime: 0.4,
+  blendTime: 0.1,
+  constrainDuringPan: true,
+  maxZoomPixelRatio: 3,
+  minZoomImageRatio: 0.25,
+  visibilityRatio: 0.1,
+  zoomPerScroll: 1.4,
+  debugMode: false,
+  gestureSettingsMouse: {
+    clickToZoom: false
+  },
+  // showNavigator: true,
+  // navigatorPosition: 'BOTTOM_LEFT',
+  navigatorHeight: 150,
+  navigatorWidth: 150,
+  showNavigationControl: false
+};
+
+OpenSeadragon(options);
